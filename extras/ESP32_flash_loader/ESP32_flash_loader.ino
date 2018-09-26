0163aa1115b9bb677a877057a8e94a931e398046
Juan Gallostra <juangallostra@gmail.com>
2018-09-26 12:28:20 +0200

    Add ESP32 flash loader

---
+++ extras/ESP32_flash_loader/ESP32_flash_loader.ino
@@ -0,0 +1,9 @@
+void setup() {
+ Serial.begin(115200);
+ Serial4.begin(115200);
+}
+
+void loop() {
+  while ( Serial4.available() ) { Serial.write(Serial4.read() ); }
+  while ( Serial.available() ) { Serial4.write(Serial.read() ); }
+}
