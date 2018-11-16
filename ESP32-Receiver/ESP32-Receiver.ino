28739943c31733d098a4912e8390cf407ce58f31
Juan Gallostra <juangallostra@gmail.com>
2018-11-16 14:43:18 +0100

    Implement low battery check

--- ESP32-Receiver/ESP32-Receiver.ino
+++ ESP32-Receiver/ESP32-Receiver.ino
@@ -1,7 +1,18 @@
 #include <WiFi.h>
 
 const int BATTERY_PIN = 33; // specific to BonaDrone FC board
-bool unknownBatteryType = true;
+// Low battery depends on the number of cells he battery has.
+// 1S operative range (3.5V-4.2V)
+// 2S operative range (7V-8.3V)
+// 3S operative range (10.5V-12.5V)
+// Since operative ranges do not overlap, we can determine
+// the type of battery being used from the first reading
+// and set the low battery limit accordingly. The values
+// defined below assume that 5V = 1023
+const int LOW_BATTERY_1S = 717;   // 3.5V for 1S batteries
+const int LOW_BATTERY_2S = 1434;  // 7.0V for 2S batteries
+const int LOW_BATTERY_3S = 2151;  // 10.5V for 3S batteries
+
 int _lowBattery = 0;
 
 // The ESP32 default IP address is 192.168.4.1
@@ -15,6 +26,27 @@ const char *password = "12345678";
 
 // Set web server port number to 80
 WiFiServer server(80);
+
+void setLowBatteryLimit(void)
+{
+    int batteryStatus = analogRead(BATTERY_PIN);
+    if (batteryStatus > LOW_BATTERY_3S)
+    {
+        _lowBattery = LOW_BATTERY_3S;
+        return;
+    }
+    else if (batteryStatus > LOW_BATTERY_2S)
+    {
+        _lowBattery = LOW_BATTERY_2S;
+        return;
+    }
+    else if (batteryStatus > LOW_BATTERY_1S)
+    {
+        _lowBattery = LOW_BATTERY_1S;
+        return;
+    }
+    // XXX What if none of the above criteria is met?
+}
 
 void checkSignal(bool hadClient)
 {
@@ -35,7 +67,7 @@ void checkSignal(bool hadClient)
 
 void checkBattery(void)
 {
-    if (analogRead(BATTERY_PIN) < _lowBattery)
+    if (analogRead(BATTERY_PIN) <= _lowBattery)
     {
         // trigger low battery message (currently Lost signal)
         // The message ID is 223 and the full message is
@@ -55,12 +87,7 @@ void setup()
     Serial.begin(115200);
     pinMode(BATTERY_PIN, INPUT);
     // Wait until type of battery (number of cells) is received
-    while (unknownBatteryType)
-    {
-      // XXX To be implemented
-      unknownBatteryType = false;
-      _lowBattery = 0;
-    }
+    setLowBatteryLimit();
     // Connect to Wi-Fi network with SSID and password
     // Remove the password parameter, if you want the AP (Access Point) to be open
     WiFi.softAP(ssid, password);
